2564345021a209c2c651d0eb41f11f7b958fc797
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-05-09 16:05:50 +0300

    062 why components need lifecycle hooks

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -18,6 +18,7 @@ export default class RandomPlanet extends Component {
   constructor() {
     super();
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 2500)
   }
 
   onPlanetLoaded = (planet) => {
@@ -31,13 +32,14 @@ export default class RandomPlanet extends Component {
     });
   }
 
-  updatePlanet() {
-    const id = Math.floor(Math.random() * 25) + 2;
+  updatePlanet = () => {
+    console.log("update");
+    const id = Math.floor(Math.random() * 25) + 3;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
       .catch(this.onError);
-  }
+  };
 
   render() {
 
