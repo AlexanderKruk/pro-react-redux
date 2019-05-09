c72262357a05e93e914a587adcd285c583443778
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-05-09 13:34:32 +0300

    fix random planet id

--- star-db/src/components/random-planet/random-planet.js
+++ star-db/src/components/random-planet/random-planet.js
@@ -32,8 +32,7 @@ export default class RandomPlanet extends Component {
   }
 
   updatePlanet() {
-    // const id = Math.floor(Math.random() * 25) + 2;
-    const id = 15000;
+    const id = Math.floor(Math.random() * 25) + 2;
     this.swapiService
       .getPlanet(id)
       .then(this.onPlanetLoaded)
