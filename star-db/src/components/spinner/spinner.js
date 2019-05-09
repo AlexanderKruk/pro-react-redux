ba4db1157676ec399796122f9d876561f4ac00da
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-05-09 13:24:27 +0300

    060 proccessing network error in component

--- star-db/src/components/spinner/spinner.js
+++ star-db/src/components/spinner/spinner.js
@@ -4,7 +4,7 @@ import './spinner.css';
 
 const Spinner = () => {
   return (
-    <div className="lds-css">
+    <div className="spinner lds-css">
       <div className="lds-double-ring">
         <div></div>
         <div></div>
